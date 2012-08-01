25cae0f50f75210e0ed420abe07437ce6364a118
francesco agati <francescoagati1975@gmail.com>
2012-08-01 12:12:55 +0200

    examples

--- prova.php
+++ prova.php
@@ -1,6 +1,14 @@
 <?php
+$app = new Slim();
+$app->map("/foo/bar");
+$x->via("GET", "POST")->name("foo");
+$app->run();
+get("users/:id.json", function ($id) {
+p($id);
+}
+);
+get("users/:id.json");
 class A {
-  public function ciao() {
+  function construct() {
   }
-  ;
 }
--- prova.rb
+++ prova.rb
@@ -1,7 +1,42 @@
+#def sum3
+#  a + b
+#  p 2
+#  c+d
+#end
+
+#sum2 = ->(a,b) { return a+b }
+
+
+
+app = Slim.new()
+
+
+app.map('/foo/bar')
+
+
+
+x.via('GET', 'POST').name('foo')
+
+app.run()
+
+
+get "users/:id.json", ->(id) do 
+  p id 
+end
+
+get "users/:id.json" do |id|
+  p id
+end
+
+#get("users/:id.json") {|id| p id }
+
+#get "users/:id.json"
+#get("users/:id.json")
+
 class A
   
-  public def ciao
-  
+  def construct
+    
   end
   
 end
